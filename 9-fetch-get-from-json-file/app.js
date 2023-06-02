7b9061b38b4b9396ed75145c23085270b6f4532d
hossein <abdollahzadeh.hossein@gmail.com>
2023-06-02 11:44:08 +0330

    Fetch get from json file

---
+++ 9-fetch-get-from-json-file/app.js
@@ -0,0 +1,51 @@
+document.getElementById('get-post').addEventListener('click', getPost);
+document.getElementById('get-posts').addEventListener('click', getPosts);
+
+function getPost() {
+    fetch('post.json')
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw Error(res.status);
+            }
+        })
+        .then(post => {
+            let output = `
+            <ul>
+                <li>ID: ${post.id}</li>
+                <li>Title: ${post.title}</li>
+                <li>Body: ${post.body}</li>
+            </ul>
+            `;
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(err => console.log(err));
+}
+
+function getPosts() {
+    fetch('posts.json')
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw Error(res.status);
+            }
+        })
+        .then(posts => {
+
+            let output = "";
+            posts.forEach(post => {
+                output += `
+            <ul>
+                <li>ID: ${post.id}</li>
+                <li>Title: ${post.title}</li>
+                <li>Body: ${post.body}</li>
+            </ul>
+            `;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(err => console.log(err));
+}
+
